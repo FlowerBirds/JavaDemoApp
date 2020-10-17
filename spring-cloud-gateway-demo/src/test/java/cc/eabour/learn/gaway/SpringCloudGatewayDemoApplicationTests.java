839ec82ecc1c1099521c68b8b6522ea446f07bc2
FlowerBirds <eabour@163.com>
2020-10-17 12:07:10 +0800

    添加spring cloud gatway 实例代码
    Signed-off-by: FlowerBirds <eabour@163.com>

---
+++ spring-cloud-gateway-demo/src/test/java/cc/eabour/learn/gaway/SpringCloudGatewayDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package cc.eabour.learn.gaway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudGatewayDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
